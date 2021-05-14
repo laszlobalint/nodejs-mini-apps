ef24b5d34ecf056bb1946e648d78af6bce1060d2
Balint Laszlo <balint.laszlo@t-systems.com>
2021-05-14 13:48:43 +0200

    Correct condition on empty arg

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,6 +2,6 @@ const geocode = require("./utils/geocode");
 const weather = require("./utils/weather");
 process.env["NO_PROXY"] = "*";
 
-geocode(process.argv[2] && "Budapest", (error, coordinates) =>
+geocode(process.argv[2] || "Budapest", (error, coordinates) =>
   error ? console.log(error) : weather(coordinates, (error, data) => (error ? console.log(error) : console.log(data)))
 );
