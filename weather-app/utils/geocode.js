00ffbac9d70fcf8bd9dc9f4274fc4ff10b08cf95
Balint Laszlo <balint.laszlo@t-systems.com>
2021-05-14 13:57:45 +0200

    Destructuring objects.

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,3 +1,4 @@
+const chalk = require("chalk");
 const request = require("postman-request");
 
 const MAP_API_TOKEN = "pk.eyJ1IjoibGFzemxvLWJhbGludCIsImEiOiJja2dqbW93d2MwNGN4MnhvaDRveDE2a2g5In0.t2okNv2tyGG718dy94WLkA";
@@ -6,16 +7,16 @@ const MAP_API_BASE_URL = "https://api.mapbox.com";
 const geocode = (address, callback) => {
   const url = `${MAP_API_BASE_URL}/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${MAP_API_TOKEN}&limit=1`;
 
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body } = {}) => {
     if (error) {
       callback(chalk.red.inverse("Could not retrieve geocoding information!"), undefined);
-    } else if (response.body.error || response.body.features.length === 0) {
+    } else if (body.error || body.features.length === 0) {
       callback(chalk.red.inverse("Unable to do the forward geocoding to get the location!"), undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name,
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
       });
     }
   });
--- weather-app/utils/weather.js
+++ weather-app/utils/weather.js
@@ -4,19 +4,19 @@ const request = require("postman-request");
 const WEATHER_API_API_KEY = "1c9531829f8a635edf26944ddc1156c6";
 const WEATHER_API_BASE_URL = "http://api.weatherstack.com";
 
-const weather = (coordinates, callback) => {
-  const url = `${WEATHER_API_BASE_URL}//current?access_key=${WEATHER_API_API_KEY}&query=${coordinates.latitude},${coordinates.longitude}&units=m`;
+const weather = ({ latitude, longitude, location } = {}, callback) => {
+  const url = `${WEATHER_API_BASE_URL}//current?access_key=${WEATHER_API_API_KEY}&query=${latitude},${longitude}&units=m`;
 
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body } = {}) => {
     if (error) {
       callback(chalk.red.inverse("Could not retrieve weather information!"), undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback(chalk.red.inverse("Unable to find the location!"), undefined);
     } else {
       callback(
         undefined,
         chalk.green.inverse(
-          `Current weather in ${coordinates.location}: ${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+          `Current weather in ${location}: ${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
         )
       );
     }
