fe8cd92c8af2e56ace3570d0e1a46002b67dad1d
Balint Laszlo <balint.laszlo@t-systems.com>
2021-05-19 11:43:49 +0200

    Move from promises to asyn/await solution.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -11,54 +11,60 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch((error) => res.status(500).send(error));
+app.get("/users", async (req, res) => {
+  try {
+    res.status(200).send(await User.find({}));
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get("/users/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => {
+app.get("/users/:id", async (req, res) => {
+  try {
+    await User.findById(req.params.id).then((user) => {
       if (!user) return res.status(404).send();
 
       res.status(200).send(user);
-    })
-    .catch((error) => res.status(500).send(error));
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.post("/users", (req, res) => {
-  const user = new User(req.body);
-
-  user
-    .save()
-    .then((user) => res.status(201).send(user))
-    .catch((error) => res.status(400).send(error));
+app.post("/users", async (req, res) => {
+  try {
+    res.status(201).send(await new User(req.body).save());
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => res.status(200).send(tasks))
-    .catch((error) => res.status(500).send(error));
+app.get("/tasks", async (req, res) => {
+  try {
+    res.status(200).send(await Task.find({}));
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get("/tasks/:id", (req, res) => {
-  Task.findById(req.params.id)
-    .then((task) => {
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    await Task.findById(req.params.id).then((task) => {
       if (!task) return res.status(404).send();
 
       res.status(200).send(task);
-    })
-    .catch((error) => res.status(500).send(error));
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-
-  task
-    .save()
-    .then((task) => res.status(201).send(task))
-    .catch((error) => res.status(400).send(error));
+app.post("/tasks", async (req, res) => {
+  try {
+    res.status(201).send(await new Task(req.body).save());
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 app.listen(port, () => console.log(`Web Server has started on port ${port}!`));
